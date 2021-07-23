bc1d777002c9b79fcd40caddd85da36d5e660736
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2021-07-23 13:57:00 +0530

    308 ms

--- solutions/1937. Maximum Number of Points with Cost.cpp
+++ solutions/1937. Maximum Number of Points with Cost.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
-    long long maxPoints(vector<vector<int>>& p) {
-    vector<long long> cur_row(p[0].size()), prev_row(p[0].size());
-    for (auto &row : p) {
-        for (long long j = 0, run_max = 0; j < row.size(); ++j) {
-            run_max = max(run_max - 1, prev_row[j]);
-            cur_row[j] = run_max;
+    long long maxPoints(vector<vector<int>>& points) {
+        vector<vector<long long>> dp(points.size(), vector<long long>(points[0].size(), -1));
+        
+        for (int i = 0; i < points[0].size(); ++i) {
+            dp[0][i] = points[0][i];
         }
-        for (long long j = row.size() - 1, run_max = 0; j >= 0; --j) {
-            run_max = max(run_max - 1, prev_row[j]);
-            cur_row[j] = max(cur_row[j], run_max) + row[j];
+        
+        for (int i = 1; i < points.size(); ++i) {
+            vector<long long> left_dp(points[i].size(), -1);
+            vector<long long> right_dp(points[i].size(), -1);
+            
+            left_dp[0] = dp[i - 1][0];
+            for (int k = 1; k < points[i].size(); ++k) {
+                left_dp[k] = max(left_dp[k - 1], dp[i - 1][k] + k);
+            }
+            
+            right_dp.back() = dp[i - 1].back() - points[i].size() + 1;
+            for (int k = points[i].size() - 2; k >= 0; --k) {
+                right_dp[k] = max(right_dp[k + 1], dp[i - 1][k] - k);
+            }
+            
+            for (int j = 0; j < points[i].size(); ++j) {
+                dp[i][j] = max(left_dp[j] - j, right_dp[j] + j) + points[i][j];
+            }
         }
-        swap(cur_row, prev_row);
+        
+        long long max_ans = -1;
+        for (const auto v : dp.back()) {
+            max_ans = max(max_ans, v);
+        }
+        
+        return max_ans;
     }
-    return *max_element(begin(prev_row), end(prev_row));
-}
 };
