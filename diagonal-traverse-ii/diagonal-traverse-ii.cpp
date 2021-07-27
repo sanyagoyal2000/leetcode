6610809871fb7518009a015379cd60bae95af209
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2021-07-27 23:47:36 +0530

    Time: 328 ms (30.97%), Space: 108.5 MB (18.90%) - LeetHub

---
+++ diagonal-traverse-ii/diagonal-traverse-ii.cpp
@@ -0,0 +1,29 @@
+#define pb push_back
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+        map<int,vector<int> >m;
+vector<int>ans;
+for(int i=0;i<nums.size();i++)
+{
+for(int j=0;j<nums[i].size();j++)
+{
+m[i+j].push_back(nums[i][j]);
+}
+
+    }
+    int t=0;
+    for(auto x:m)
+    {
+        while(x.second.size()>0)
+            
+        {
+            
+            ans.push_back(x.second.back());
+            x.second.pop_back();
+        }
+        
+    }
+    return ans;
+    }
+};
