70e49a0f03287e27966a4d4533c5bbc6e8b4af07
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2021-07-29 19:42:17 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (71.39%) - LeetHub

--- pascals-triangle/pascals-triangle.cpp
+++ pascals-triangle/pascals-triangle.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    
-       vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> v(numRows);
-        v[0].assign(1 , 1);
-        if(numRows == 1) return v;
-        v[1].assign(2 , 1);
-        if(numRows == 2) return v;
-        for(int i = 2 ;i<numRows;i++){
-            v[i].assign(i+1 , 1);
-            int k = 1;
-            for(int j = 0 ;j<i-1;j++){
-                v[i][k] = v[i-1][k-1]+v[i-1][k];
-                k++;
-            }
-        }
-        return v;
+    vector<vector<int>> generate(int numRows) {
+        //ith row has i+1 col
+        vector<vector<int>>dp(numRows,vector<int>(numRows,0));
+        dp[0][0]=1;
+        dp[0].resize(1);
+        
+       for(int i=1;i<numRows;i++){
+           for(int j=0;j<=i;j++){
+               if(j==0)dp[i][j]=1;
+               else if(j==i)dp[i][j]=1;
+                else{
+                    dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
+                }   
+           }
+           dp[i].resize(i+1);
+       }
+        return dp;
     }
-    
 };
