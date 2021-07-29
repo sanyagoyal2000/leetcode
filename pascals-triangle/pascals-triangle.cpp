5a74c8f8a926073585ae6caaabe0862edb89b86f
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2021-07-29 18:12:38 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (34.72%) - LeetHub

---
+++ pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+       vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> v(numRows);
+        v[0].assign(1 , 1);
+        if(numRows == 1) return v;
+        v[1].assign(2 , 1);
+        if(numRows == 2) return v;
+        for(int i = 2 ;i<numRows;i++){
+            v[i].assign(i+1 , 1);
+            int k = 1;
+            for(int j = 0 ;j<i-1;j++){
+                v[i][k] = v[i-1][k-1]+v[i-1][k];
+                k++;
+            }
+        }
+        return v;
+    }
+    
+};
