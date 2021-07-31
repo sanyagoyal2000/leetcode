d3b34f22d105444719a8ddcc63c7af7541452802
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2021-07-31 09:30:22 +0530

    32 ms

---
+++ solutions/503. Next Greater Element II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans(n,0);
+        stack<int>s;
+        for(int i=n-1;i>=0;i--) {
+            s.push(nums[i]);
+        }
+        
+        for(int i=n-1;i>=0;i--) {
+            while(!s.empty() && nums[i]>=s.top()) s.pop();
+            if(s.empty()) ans[i]=-1;
+            else ans[i]=s.top();
+            s.push(nums[i]);
+        }
+        return ans;
+    }
+};
