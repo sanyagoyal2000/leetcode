769a1a44ce4b4fd951a96a1d4e8736ec7cfdb62d
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2021-08-02 11:29:25 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (67.89%) - LeetHub

--- spiral-matrix/spiral-matrix.cpp
+++ spiral-matrix/spiral-matrix.cpp
@@ -1,51 +1,44 @@
+#define pb push_back
 class Solution {
 public:
-    #define pb push_back
-    vector<int> spiralOrder(vector<vector<int>>& matrix) 
-    {
-        //approach according to ques 
-        //after a traversal shrink that particular row or col
-        int i, sr = 0, sc = 0; //sr-> starting row, sc->starting col
-        int row = matrix.size();
-        if(row == 0)
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int row=matrix.size();
+        int col=matrix[0].size();
+        if(row==0){
             return {};
+        }
+        vector<int>ans;
+        int sr=0,sc=0;
         
-        int col = matrix[0].size();
-        vector<int> ans;
-        while (sr < row && sc < col) 
-        { 
-            /* push the first row from 
-                   the remaining rows */
-            for (i = sc; i <= col-1; i++) 
+        while(sr<row and sc<col){
+            
+            for(int i=sc;i<=col-1;i++){
                 ans.pb(matrix[sr][i]);
-            
-            sr++; 
-
-            /* push the last column  
-             from the remaining columns */
-            for (i = sr; i <= row-1; i++) 
-                ans.pb(matrix[i][col - 1]); 
-            
-            col--; 
-
-            /* push the last row from  
-                    the remaining rows */
-            if (sr < row) 
-            { 
-                for (i = col - 1; i >= sc; i--) 
-                    ans.pb(matrix[row - 1][i]);
                 
-                row--; 
-            } 
-            /* push the first column from 
-                       the remaining columns */
-            if (sc< col) 
-            { 
-                for (i = row - 1; i >= sr; i--) 
-                    ans.pb(matrix[i][sc]);
+            }
+            sr++;
             
-                sc++; 
-            } 
+            for(int i=sr;i<=row-1;i++){
+                ans.pb(matrix[i][col-1]);
+            }
+            
+            col--;
+            
+            if(sr<row){
+                for(int i=col-1;i>=sc;i--){
+                    ans.pb(matrix[row-1][i]);
+                }
+                row--;
+            }
+            
+             if(sc<col){
+                for(int i=row-1;i>=sr;i--){
+                    ans.pb(matrix[i][sc]);
+                }
+                sc++;
+            }
+            
+            
         }
         return ans;
     }
