cac937f617d7ecfdf32131981fdef6770dc5e6ca
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2021-08-02 13:22:18 +0530

    Time: 4 ms (88.10%), Space: 9.8 MB (49.69%) - LeetHub

---
+++ gas-station/gas-station.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        // support variables
+        int len = gas.size(), curr = 0, total = 0, res = 0;
+        // looking for the index
+        for (int i = 0, n; i < len; i++) {
+            n = gas[i] - cost[i];
+            // updating res
+            if (curr <= 0 && n > 0) {
+                res = i;
+                curr = 0;
+            }
+            curr += n;
+            total += n;
+        }
+        return total >= 0 ? res : -1;
+    }
+};
