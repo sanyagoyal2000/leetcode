5413bd275b460cd4008945cfe524efe377abe1d5
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2021-08-11 09:09:33 +0530

    Time: 220 ms (14.69%), Space: 90.3 MB (11.53%) - LeetHub

---
+++ best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int dp[100001][2][3];
+    int solve(int i,vector<int>& prices,bool own,int k){
+        if(i==prices.size() or k==0){
+            return 0;
+        }
+        if(dp[i][own][k]!=-1){
+            return dp[i][own][k];
+        }
+        else if(!own){
+            int op1=solve(i+1,prices,1,k)-prices[i];
+            int op2=solve(i+1,prices,0,k);
+            return dp[i][own][k]=max(op1,op2);
+        }
+        else{
+            int op1=prices[i]+solve(i+1,prices,0,k-1);
+            int op2=solve(i+1,prices,1,k);
+             return dp[i][own][k]=max(op1,op2);
+        }
+    }
+    int maxProfit(vector<int>& prices) {
+        int k=2;
+        memset(dp,-1,sizeof(dp));
+        return solve(0,prices,0,k);
+    }
+};
