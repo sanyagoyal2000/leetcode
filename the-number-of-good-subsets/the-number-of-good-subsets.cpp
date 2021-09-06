ff3b04658925f3241fff43df166ae3f789f2ec4d
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2021-09-06 11:55:07 +0530

    Time: 276 ms (69.23%), Space: 184.5 MB (69.23%) - LeetHub

--- the-number-of-good-subsets/the-number-of-good-subsets.cpp
+++ the-number-of-good-subsets/the-number-of-good-subsets.cpp
@@ -1,94 +1,52 @@
-/*
-https://leetcode.com/problems/the-number-of-good-subsets/discuss/1444481/C%2B%2B-DP-%2B-BITMASKING-or-EASY-EXPLANATION-or-SIMPLE-CODE*/
+#define vi vector<int>
+#define ll long long
 
+ll mod = 1e9+7;
 class Solution {
 public:
-    
-    #define lld long long int
-    
-    int mul(lld a, lld b){
-        lld product = (a*b)%MOD;
-        return product;
-    }
-    
-    int add(lld a, lld b){
-        lld addition = (a+b)%MOD;
-        return addition;
-    }
-    
-    const int MOD = 1e9+7;
-    int binary_exponentiation(lld x, int p){
-        long long res = 1;
-        while(p){
-            if(p&1) res = mul(res, x);
-            x = mul(x, x);
-            p/=2;
+    int numberOfGoodSubsets(vi& nums) {
+        
+        unordered_map<int, int>m;
+        int ones = 0, res=0;
+        
+        int prime[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+        
+        vi dp(1<<10, 0), tmp;
+        dp[0] = 1; //everything is absent
+        
+        for(auto x:nums){
+            if(x%4==0 || x%9==0 || x%25==0) continue;  //exclude square terms
+            if(x==1) ones++;                           //special dealing
+            else m[x]++;
+        }
+        
+        for(auto x:m){
+            
+            //find current mask
+            int curr_mask = 0;
+            for(int i=0; i<10; i++)
+                if(x.first%prime[i]==0) 
+                    curr_mask+=(1<<i);
+            
+            //state updates
+            tmp.assign(1<<10, 0);
+            for(int mask=0; mask<(1<<10); mask++)
+                if( (curr_mask & mask) ==0)
+                    tmp[mask | curr_mask] = ((ll) dp[mask]*(ll) x.second)%mod;  //x.second is frequency
+            
+            //add tmp to dp
+            for(int i=0; i<(1<<10); i++)
+                dp[i] = (dp[i]+tmp[i] )%mod;
+            
+        }
+        
+        //remember to exclude empty set
+        for(int i=1; i<(1<<10); i++) res =(res+dp[i] )%mod;
+        
+        //deal with ones
+        while(ones--){
+            res = ( (ll)res*2LL)%mod;
         }
         return res;
-    }
-    
-    int goodSubsets(int pos, int mask, vector<int>& V, vector<vector<int>>& dp, vector<int>& cache){
-        if(pos == V.size()) return (mask>0);
-        
-        if(dp[pos][mask] != -1) return dp[pos][mask]%MOD;
-        
-        if(V[pos]&mask) return dp[pos][mask] = goodSubsets(pos+1, mask, V, dp, cache) % MOD;
-        return dp[pos][mask] = add(mul(cache[V[pos]],goodSubsets(pos+1, mask|V[pos], V, dp, cache)),goodSubsets(pos+1, mask, V, dp, cache));
-    }
-    
-    int numberOfGoodSubsets(vector<int>& nums) {
-        
-        int primes[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
-        
-        vector<int> V;
-        vector<int> cache(1025, 0);
-        
-        int ones = 0;
-        
-        for(auto x: nums){
-            
-            int num = 0, k=0;
-            int flag = 1;
-            
-            for(auto j: primes){
-
-                int cnt = 0;
-                while(x%j == 0){
-                    x/=j;
-                    cnt++;
-					if(cnt>1) break;
-                }
-                
-                if(cnt > 1)
-                { 
-                    flag = 0;
-                    break;
-                }
-                
-                if(cnt == 1)
-                    num = num | (1<<k);
-                
-                ++k;
-            }
-            
-            if(flag == 0) continue;
-            
-            if(num == 0) { 
-                ones++;
-                continue;
-            }
-            
-            cache[num]++;
-            if(cache[num] > 1) continue;
-            
-            V.push_back(num);
-        }
-        
-        vector<vector<int>> dp(V.size(), vector<int> (1024, -1));
-        
-        int ans = goodSubsets(0,0,V,dp,cache);
-        ans = mul(binary_exponentiation(2, ones),ans);
-        
-        return ans;
     }
 };
