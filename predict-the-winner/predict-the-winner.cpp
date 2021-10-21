1e374d3a435470c0d866f0be8fbfe7fb3fa728d8
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2021-10-21 14:49:01 +0530

    Time: 7 ms (38.56%), Space: 7.9 MB (14.73%) - LeetHub

--- predict-the-winner/predict-the-winner.cpp
+++ predict-the-winner/predict-the-winner.cpp
@@ -1,5 +1,34 @@
 class Solution {
 public:
+    bool PredictTheWinner(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int g=0;g<n;g++){
+            for(int i=0,j=g;j<n;i++,j++){
+                if(g==0)dp[i][j]=nums[i];
+                else if(g==1)dp[i][j]=max(nums[i],nums[j]);
+                else{
+                    int val1=nums[i]+min(dp[i+2][j],dp[i+1][j-1]);
+                    int val2=nums[j]+min(dp[i+1][j-1],dp[i][j-2]);
+                    dp[i][j]=max(val1,val2);
+                }
+            }
+        }
+        int p=dp[0][n-1];
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        if(n==1)return true;
+        
+        if(p>=(sum-p))return true;
+        return false;
+    }
+};
+
+/*
+class Solution {
+public:
     int dfs(vector<int>& nums,int s,int e){
         if(s==e or s==(e-1)){
             return max(nums[s],nums[e]);
@@ -20,3 +49,4 @@ public:
         return false;
     }
 };
+*/
