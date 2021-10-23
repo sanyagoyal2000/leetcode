28189e10c2a5b257753166fd40cb50d55817c53a
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2021-10-23 23:35:39 +0530

    Time: 100 ms (73.72%), Space: 73.3 MB (27.81%) - LeetHub

---
+++ find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -0,0 +1,29 @@
+typedef pair<int, pair<int, int>> pip;
+class Solution {
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+            // make sure the array should is sorted as need smallest first
+        
+            int n1 = nums1.size(), n2 = nums2.size();  vector<vector<int>> ans;
+        
+            priority_queue<pip, vector<pip>, greater<pip>> minheap;
+        
+            for(int i=0; i<n1; i++)
+                minheap.push({nums1[i] + nums2[0], {i,0}});
+        
+            while(ans.size() < k && !minheap.empty()){
+                
+                pair<int, pair<int, int>> p = minheap.top();
+                minheap.pop();
+                
+                int i = p.second.first, j = p.second.second;
+                ans.push_back({nums1[i],nums2[j]});
+               // ans.push_back(p.first);  // if sum is asked instead of pairs
+                         
+                if(j+1 < n2){
+                    minheap.push({nums1[i]+nums2[j+1], {i, j+1}});
+                }
+        }
+        return ans;
+    }
+};
