ee7677d19510d40acaf7cf5ce3ee4e6ee9593196
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-01-10 15:32:08 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (14.80%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.cpp
+++ 1405-longest-happy-string/1405-longest-happy-string.cpp
@@ -1,8 +1,42 @@
 class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
-        string res="";
-        int size = a + b + c;
+        //string res="";
+        priority_queue<pair<int, char>> pq;
+        if(a != 0) pq.push({ a, 'a' });
+        if(b != 0) pq.push({ b, 'b' });
+        if(c != 0) pq.push({ c, 'c' });
+        char prev1 = '#';
+        char prev2 = '#';
+        string res;
+        while(!pq.empty())
+        {
+            auto [cnt1, ch1] = pq.top(); pq.pop();
+            if(ch1 == prev1 && ch1 == prev2)
+            {
+                if(pq.empty()) return res;
+                auto [cnt2, ch2] = pq.top(); pq.pop();
+                res += ch2;
+                prev1 = prev2;
+                prev2 = ch2;
+                pq.push({ cnt1, ch1 });
+                if(--cnt2 > 0) pq.push({ cnt2, ch2 });
+            }
+            else
+            {
+                prev1 = prev2;
+                prev2 = ch1;
+                res += ch1;
+                if(--cnt1 > 0) pq.push({ cnt1, ch1 });
+            }
+        }
+        return res;
+    }
+    
+};
+
+/*
+int size = a + b + c;
         int A = 0, B = 0, C = 0;
         for(int i = 0; i < size; i++) {
             if ((a >= b && a >= c && A != 2) || (B == 2 && a > 0) || (C == 2 && a > 0)){
@@ -17,5 +51,8 @@ public:
             }
         }
         return res;
-    }
+
 };
+
+
+*/
