fd2b7788426fba2a7eea9438782626e52deb0f1a
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-04-02 19:16:26 +0530

    Time: 27 ms (62.77%), Space: 11.7 MB (44.92%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                st.push(i);
+            }
+            else if(s[i]==')'){
+                if(!st.empty()){
+                    st.pop();
+                }
+                else{
+                    s[i]='#';
+                }
+            }
+        }
+        while(!st.empty()){
+            int x=st.top();
+            s[x]='#';
+            st.pop();
+        }
+        string res="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]!='#'){
+                res+=s[i];
+            }
+        }
+        return res;
+    }
+};
