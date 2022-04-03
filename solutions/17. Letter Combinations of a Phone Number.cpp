54835292cc279cc098677caad8f22db7e85e5e6b
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-04-03 11:10:38 +0530

    0 ms

---
+++ solutions/17. Letter Combinations of a Phone Number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string>v={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    
+    vector<string>res;
+    void dp(int i,int n,string s,string &digits){
+        if(i==n){
+            res.push_back(s);
+            return;
+        }
+        int curr=digits[i]-'0';
+        if(curr==0 or curr==1){
+            dp(i+1,n,s,digits);
+        }
+        for(int k=0;k<v[curr].size();k++){
+            dp(i+1,n,s+v[curr][k],digits);
+        }
+        return;
+    }
+    
+    vector<string> letterCombinations(string digits) {
+        int n=digits.size();
+        if(n==0)return res;
+        dp(0,n,"",digits);
+        return res;
+    }
+};
