dc150ec1a767218ef8606d7847386a016f4ef311
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-04-03 18:00:42 +0530

    Time: 7 ms (80.46%), Space: 11.1 MB (52.19%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    int len;
+    void dfs ( vector<int> &candidates, int target, vector<int> &cur_path, int start )
+	{
+		if ( target < 0 ) return;
+		if ( target == 0 )
+		{
+			ans.push_back(cur_path);
+			return;
+		}
+		
+		for ( ; start < len; ++start )
+		{
+			cur_path.push_back( candidates[start] );
+			dfs ( candidates, target - candidates[start], cur_path, start );
+			cur_path.pop_back();
+		}
+	}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+       vector<int>s;
+        len=candidates.size();
+        dfs(candidates,target,s,0);
+        return ans;
+    }
+};
