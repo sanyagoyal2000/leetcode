41fce968be664262bcc7b3fa386eadebfedb6a71
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-05-11 18:44:02 +0530

    16 ms

--- solutions/34. Find First and Last Position of Element in Sorted Array.cpp
+++ solutions/34. Find First and Last Position of Element in Sorted Array.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
+    int bs(vector<int>&nums,int k,bool flag){
+        int l=0;
+        int r=nums.size()-1;
+        int ans=-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(nums[mid]==k){
+                ans=mid;
+                (flag==true)?r=mid-1:l=mid+1;
+            }
+            else if(nums[mid]>k)r=mid-1;
+            else l=mid+1;
+        }
+        return ans;
+    }
+   
     vector<int> searchRange(vector<int>& nums, int target) {
-     
-vector<int>ans(2,-1);
-     int n=nums.size();
-        if(n==0 or nums[n-1]<target)return ans;
-     auto it1=lower_bound(nums.begin(),nums.end(),target);
-     auto it2=upper_bound(nums.begin(),nums.end(),target);
-      
-      int k=nums[it1-nums.begin()];
-        if(k!=target){
-            return ans;
-        }
-      
-      
-          int p1=it1-nums.begin();
-          int p2=it2 -nums.begin()-1;
-          
-              ans[0]=p1;
-              ans[1]=p2;
-          return ans;
-}
+        vector<int>ans={-1,-1};
+        
+        if(nums.size()==0 or nums[nums.size()-1]<target)return ans;
+        
+        ans[0]=(bs(nums,target,true));
+        ans[1]=(bs(nums,target,false));
+        return ans;
+​
+    }
 };
+​
