f957bc938541b983c4960040355ea7de520a3c72
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-05-13 10:33:30 +0530

    32 ms

--- solutions/4. Median of Two Sorted Arrays.cpp
+++ solutions/4. Median of Two Sorted Arrays.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-      vector<int>& smaller = nums1.size() < nums2.size() ? nums1 : nums2;
-      vector<int>& larger = nums1.size() < nums2.size() ? nums2 : nums1;
-      auto begin = larger.begin();
-      
-      for(int n : smaller) {
-        auto pos_iter = std::upper_bound(begin, larger.end(), n);
-        begin = larger.insert(pos_iter, n);
-      }
-      
-      if(larger.size() % 2 == 0) {
-        const size_t iprev = larger.size()/2-1;
-        const size_t inext = larger.size()/2;
-        return 0.5*larger[iprev] + 0.5*larger[inext];
-      } else {
-        const size_t index = (larger.size()-1)/2;
-        return larger[index];
-      }
-      return 0.0;
-    }
+   
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+        if(nums2.size()<nums1.size())return findMedianSortedArrays(nums2,nums1);
+        int m=nums1.size();
+        int n=nums2.size();
+        int t=(m+n+1)/2;
+        
+        
+            int l=0;
+            int r=m;
+            while(l<=r){
+                int cut1=(l+r)/2;
+                int cut2=(m+n+1)/2-cut1;
+                 int l1=(cut1>0)?nums1[cut1-1]:INT_MIN;
+                 int r1=(cut1<m)?nums1[cut1]:INT_MAX;
+                int l2=(cut2>0)?nums2[cut2-1]:INT_MIN;
+             int r2=(cut2<n)?nums2[cut2]:INT_MAX;
+                if(l1<=r2 and l2<=r1){
+                    return((m+n)%2==0)?(max(l1,l2)+min(r1,r2)/2.0):(max(l1,l2));
+                }
+                else if(l1>r2){
+                    r=cut1-1;
+                }
+                else if(l2>r1){
+                    l=cut1+1;
+                }
+            }
+            return 0.0;
+        }
+    
 };
