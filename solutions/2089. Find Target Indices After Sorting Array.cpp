0b21beb9fc1531398d9c4acc341071b94246485f
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-05-13 10:58:27 +0530

    7 ms

---
+++ solutions/2089. Find Target Indices After Sorting Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+   vector<int> targetIndices(vector<int>& nums, int target) {
+        int small=0,count=0,k=0;
+        for(auto n:nums) {
+            if(n<target)    small++;                                        // calculates the frequency of elements lesser than the target
+            if(n==target)   count++;                                        // calculate the number of occurences of the target element
+        }
+        vector<int> result(count);
+        for(int i=small;i<small+count;i++)      
+            result[k++]=i;
+        return result;
+    }
+};
