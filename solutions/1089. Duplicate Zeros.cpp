39ecb546a132cf53667043dfc5b8766fe31124f6
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-05-13 14:17:28 +0530

    8 ms

---
+++ solutions/1089. Duplicate Zeros.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        queue<int>q;
+        int i;
+        for( i=0;i<arr.size();i++){
+            if(arr[i]!=0)q.push(arr[i]);
+            else{ q.push(0);q.push(0);}
+            int x=q.front();
+            q.pop();
+            arr[i]=x;
+        }
+      
+    }
+};
