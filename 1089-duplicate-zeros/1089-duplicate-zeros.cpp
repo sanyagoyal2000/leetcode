e643201c9ff40553a00e2c0ecb246788744b2194
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-05-13 14:17:23 +0530

    Time: 8 ms (69.98%), Space: 10 MB (20.69%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
-    void duplicateZeros(vector<int>& a, int i = 0, int sh = 0) {
-  for (i = 0; sh + i < a.size(); ++i) sh += a[i] == 0;
-  for (i = i - 1; sh > 0; --i) {
-    if (i + sh < a.size()) a[i + sh] = a[i];
-    if (a[i] == 0) a[i + --sh] = a[i];
-  }
-}
+    void duplicateZeros(vector<int>& arr) {
+        queue<int>q;
+        int i;
+        for( i=0;i<arr.size();i++){
+            if(arr[i]!=0)q.push(arr[i]);
+            else{ q.push(0);q.push(0);}
+            int x=q.front();
+            q.pop();
+            arr[i]=x;
+        }
+      
+    }
 };
