3a65a0a607439c1b2f5fef6bea96ecf7110b2444
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-05-14 12:52:08 +0530

    541 ms

---
+++ solutions/2226. Maximum Candies Allocated to K Children.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    typedef long long ll;
+    
+    bool solve(vector<int>& v, ll mid, ll k){
+        int n = v.size();
+        ll cnt = 0;
+        for(int i=0;i<n;i++){
+            cnt += (v[i]/mid);
+            if(cnt>=k) return true;
+        }
+        return false;
+    }
+    
+    // v is the candies vector.
+    int maximumCandies(vector<int>& v, long long k) {
+        int n = v.size();
+        int mx = 0;
+        for(int i=0;i<n;i++){
+            mx = max(mx,v[i]);
+        }
+        
+        ll low = 1, high = mx;
+        ll ans = 0;
+        while(low<=high){
+            ll mid = low + (high-low)/2;
+            if(solve(v,mid,k)){
+                ans = mid;
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+};
