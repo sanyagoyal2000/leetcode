46ddc7d55b1bf092136abb587111c6c4f375eea5
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-06-05 19:16:04 +0530

    668 ms

---
+++ solutions/1721. Swapping Nodes in a Linked List.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode *begin;
+        ListNode *slow=head ,*fast=head;
+        
+        for(int cnt=1;cnt<=k and fast!=NULL;cnt++){
+            begin=fast;
+            fast=fast->next;
+        }
+        while(fast!=NULL){
+            slow=slow->next;
+            fast=fast->next;
+        }
+        int nk=slow->val;
+        slow->val=begin->val;
+        begin->val=nk;
+        return head;
+        
+    }
+};
