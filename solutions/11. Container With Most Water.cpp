25c96f130694456939728d6a71980315d25f7c2d
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-06-05 19:10:25 +0530

    101 ms

---
+++ solutions/11. Container With Most Water.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int ans=0;
+        int res=0;
+        int i=0;
+        int j=height.size()-1;
+        while(i<j){
+            if(height[i]<=height[j]){
+                ans=(j-i)*height[i];
+                i++;
+            }
+            else{
+                ans=(j-i)* height[j];
+                j--;
+            }
+            res=max(ans,res);
+        }
+        return res;
+    }
+};
