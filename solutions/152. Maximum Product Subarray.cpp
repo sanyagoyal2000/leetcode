ec38d37a65f2b7a5c3a0eca296222f12c02fdce9
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-08-03 10:26:08 +0530

    15 ms

--- solutions/152. Maximum Product Subarray.cpp
+++ solutions/152. Maximum Product Subarray.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-            int maxProduct = nums[0];
-    int imax = nums[0];
-    int imin = nums[0];
-    for(int i=1 ;i<nums.size();i++) {
-        if(nums[i]<0)
-            swap(imax,imin);
-        imax = max(nums[i], imax * nums[i]);
-        imin = min(nums[i],imin * nums[i]);
-        maxProduct = max(maxProduct, imax);
-    }
-    return maxProduct;
+        int ans=nums[0];
+        int maxe=nums[0];
+        int mine=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<0){
+                swap(maxe,mine);
+            }
+            maxe=max(nums[i],(maxe*nums[i]));
+            mine=min(nums[i],(mine*nums[i]));
+            ans=max(ans,maxe);
+        }
+        return ans;
     }
 };
