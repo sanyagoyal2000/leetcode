ec8c5cddc84dc7f128f1cc9c27deb582d6b6a306
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-08-18 22:35:38 +0530

    Time: 10 ms (56.27%), Space: 13.1 MB (44.83%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,45 +9,50 @@
  */
 class Solution {
 public:
-    vector<int> ans;   
-    map<TreeNode*, TreeNode*> parent;  // son=>parent  
-    set<TreeNode*> visit;    //record visied node
-    
-    void findParent(TreeNode* node){
-        if(!node ) return;
-        if( node->left ){
-            parent[node->left] = node;
-            findParent(node->left);
-        }
-        if( node->right){
-            parent[node->right] = node;
-            findParent(node->right);
+    void markParent(TreeNode *root,map<TreeNode*,TreeNode*>&par){
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode *temp=q.front();
+            q.pop();
+            if(temp->left){
+                par[temp->left]=temp;
+                q.push(temp->left);
+                
+            }
+             if(temp->right){
+                par[temp->right]=temp;
+                q.push(temp->right);
+                
+            }
         }
     }
-    
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int K) {
-        if( !root ) return {};
-        
-        findParent(root);
-        dfs(target, K );
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<int>ans;
+        map<TreeNode*,TreeNode*>par;
+        markParent(root,par);
+        set<TreeNode*>vis;
+        vis.insert(target);
+        queue<pair<TreeNode*,int>>q;
+        q.push({target,0});
+        while(!q.empty()){
+            auto[node,dis]=q.front();
+            if(dis==k){ans.push_back(node->val);}
+            if(dis>k)break;
+            q.pop();
+            if(node->left and vis.find(node->left)==vis.end()){
+                vis.insert(node->left);
+                q.push({node->left,dis+1});
+            }
+            if(node->right and vis.find(node->right)==vis.end()){
+                vis.insert(node->right);
+                q.push({node->right,dis+1});
+            }
+            if(par[node] and vis.find(par[node])==vis.end()){
+                vis.insert(par[node]);
+                q.push({par[node],dis+1});
+            }
+        }
         return ans;
-    }
-    void dfs( TreeNode* node, int K){
-        if( visit.find(node) != visit.end() )
-            return;
-        visit.insert(node);
-        if( K == 0 ){
-            ans.push_back(node->val);
-            return;
-        }
-        if( node->left ){
-            dfs(node->left, K-1);
-        }
-        if( node->right){
-            dfs(node->right, K-1);
-        }
-        TreeNode* p = parent[node];
-        if( p )
-            dfs(p, K-1);
     }
 };
