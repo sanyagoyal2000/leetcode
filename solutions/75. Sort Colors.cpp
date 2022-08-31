855a2c9248ab77b3f813e6f7389eb6aad5faac5c
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-08-31 22:14:21 +0530

    9 ms

---
+++ solutions/75. Sort Colors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int mid = 0, low = 0, high = nums.size() - 1;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[mid], nums[low]);
+                mid++;
+                low++;
+            }
+            else if(nums[mid] == 2){
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+            else mid++;
+        }
+    }
+};
