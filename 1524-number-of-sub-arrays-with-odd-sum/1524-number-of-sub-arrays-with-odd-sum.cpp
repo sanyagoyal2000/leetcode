c491286b40d3a3105e7efa70b20b9857fd6c5bde
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-09-04 22:52:16 +0530

    Time: 218 ms (69.03%), Space: 108.1 MB (37.07%) - LeetHub

---
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int,int>m;
+        int ans=0;
+        m[0]=1;
+        int sum=0;
+        int rem=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            rem=sum%k;
+            if(rem<0){
+                rem+=k;
+            }
+            if(m.find(rem)!=m.end()){
+                ans+=m[rem];
+                m[rem]++;
+            }
+            else m[rem]++;
+        }
+        return ans;
+    }
+    long long mod=(1000000000 + 7);
+    int numOfSubarrays(vector<int>& arr) {
+        
+        long long  count=0;
+        long long  sum=0;
+        
+        int k=2;  
+        int n=arr.size();
+        unordered_map<int,int> mp;
+        mp[0]=1; 
+        
+        for(int i=0;i<arr.size();i++){
+            
+            sum+=arr[i];
+             int re=sum%k;
+         
+            
+            if (mp.find(re)!=mp.end()){
+                count=count + mp[re];
+            }
+            
+            mp[re]++;
+        }
+        
+       long long m= ((n%mod)*((n+1)%mod))/2; 
+        
+        
+        return (m-count)%mod;
+    }
+};
