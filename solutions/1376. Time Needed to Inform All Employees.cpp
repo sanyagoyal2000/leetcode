40457cddb779e6f7e2dd3a55dba8ad3200fb9b0a
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-09-09 09:26:21 +0530

    N/A

--- solutions/1376. Time Needed to Inform All Employees.cpp
+++ solutions/1376. Time Needed to Inform All Employees.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    vector<int>adj[100005];
-    int dfs(int headID,vector<int>&informTime){
-        int maxi=0;
-        for(auto nbr:adj[headID]){
-            maxi=max(maxi,dfs(headID,informTime));
+    int util(vector<int>& manager, vector<int>& informTime, int s, vector<int> &dp){
+        if(manager[s]==-1){
+            return 0;
         }
-        
-        return informTime[headID]+maxi;
+        if(dp[s]!=-1){
+            return dp[s];
+        }
+        dp[s]=informTime[manager[s]]+util(manager, informTime, manager[s], dp);
+        return dp[s];
     }
-    
-    
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        for(int i=0;i<manager.size();i++){
-            if(manager[i]!=-1){
-                adj[manager[i]].push_back(i);
+        vector<int> dp(n,-1);
+        int i;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(dp[i]==-1){
+                dp[i]=util(manager, informTime, i, dp);
             }
+            ans=max(ans, dp[i]);
         }
-        
-        return dfs(headID,informTime);
-       
+        return ans;
     }
 };
