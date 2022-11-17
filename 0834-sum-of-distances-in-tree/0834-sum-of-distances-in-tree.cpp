8c6fc4dd13956a52d9b8666508d7f02a2e7cd2ab
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-11-17 23:35:07 +0530

    Time: 522 ms (66.11%), Space: 85.1 MB (87.59%) - LeetHub

---
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+     void preprocessing(vector<int>adj[],int src,int par,vector<int>&subTreeSize,vector<int>&subTreeAns){
+        int noNodes=1;
+         int ansNodes=0;
+        for(auto child:adj[src]){
+            if(child!=par){
+                preprocessing(adj,child,src,subTreeSize,subTreeAns);
+                noNodes+=subTreeSize[child];
+                ansNodes+=subTreeAns[child]+subTreeSize[child];
+            }
+        }
+         subTreeAns[src]=ansNodes;
+         subTreeSize[src]=noNodes;
+        
+    }
+    
+    void dfs(vector<int>adj[],int src,int par,vector<int>&subTreeSize,vector<int>&subTreeAns,int partial_ans,vector<int>&ans,int n){
+        ans[src]=subTreeAns[src]+partial_ans+(n-subTreeSize[src]);
+        for(auto child:adj[src]){
+            if(child!=par){
+                dfs(adj,child,src,subTreeSize,subTreeAns,ans[src]-subTreeAns[child]-subTreeSize[child],ans,n);
+            }
+        }
+        
+        
+    }
+    
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        vector<int>adj[n];
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        vector<int>subTreeSize(n,0);
+        vector<int>subTreeAns(n,0);
+        preprocessing(adj,0,-1,subTreeSize,subTreeAns);
+        vector<int>dis(n,0);
+        dfs(adj,0,-1,subTreeSize,subTreeAns,0,dis,n);
+        return dis;
+    }
+};
