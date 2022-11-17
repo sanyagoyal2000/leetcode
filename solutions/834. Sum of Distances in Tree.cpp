f095547bee955bdc5ee8e1798df9ea0326e8e1ce
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-11-18 00:09:04 +0530

    522 ms

---
+++ solutions/834. Sum of Distances in Tree.cpp
@@ -0,0 +1,48 @@
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        vector<int>subTreeSize(n,0);
+        vector<int>subTreeAns(n,0);
+        preprocessing(adj,0,-1,subTreeSize,subTreeAns);
+        vector<int>dis(n,0);
+        dfs(adj,0,-1,subTreeSize,subTreeAns,0,dis,n);
+        return dis;
+    }
+    */
+      vector<unordered_set<int>> tree;
+    vector<int> res, count;
+​
+    vector<int> sumOfDistancesInTree(int N, vector<vector<int>>& edges) {
+        tree.resize(N);
+        res.assign(N, 0);
+        count.assign(N, 1);
+        for (auto e : edges) {
+            tree[e[0]].insert(e[1]);
+            tree[e[1]].insert(e[0]);
+        }
+        dfs(0, -1);
+        dfs2(0, -1);
+        return res;
+​
+    }
+​
+    void dfs(int root, int pre) {
+        for (auto i : tree[root]) {
+            if (i == pre) continue;
+            dfs(i, root);
+            count[root] += count[i];
+            res[root] += res[i] + count[i];
+        }
+    }
+​
+    void dfs2(int root, int pre) {
+        for (auto i : tree[root]) {
+            if (i == pre) continue;
+            res[i] = res[root] - count[i] + count.size() - count[i];
+            dfs2(i, root);
+        }
+    }
+};
