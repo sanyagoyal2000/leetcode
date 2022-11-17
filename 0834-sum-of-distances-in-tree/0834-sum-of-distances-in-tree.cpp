a026411b67f9ffe3235ce297d44de07038e8dea8
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-11-18 00:09:16 +0530

    Time: 532 ms (61.82%), Space: 131.2 MB (7.04%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-     void preprocessing(vector<int>adj[],int src,int par,vector<int>&subTreeSize,vector<int>&subTreeAns){
+     /*void preprocessing(vector<int>adj[],int src,int par,vector<int>&subTreeSize,vector<int>&subTreeAns){
         int noNodes=1;
          int ansNodes=0;
         for(auto child:adj[src]){
@@ -40,5 +40,39 @@ public:
         vector<int>dis(n,0);
         dfs(adj,0,-1,subTreeSize,subTreeAns,0,dis,n);
         return dis;
+    }
+    */
+      vector<unordered_set<int>> tree;
+    vector<int> res, count;
+
+    vector<int> sumOfDistancesInTree(int N, vector<vector<int>>& edges) {
+        tree.resize(N);
+        res.assign(N, 0);
+        count.assign(N, 1);
+        for (auto e : edges) {
+            tree[e[0]].insert(e[1]);
+            tree[e[1]].insert(e[0]);
+        }
+        dfs(0, -1);
+        dfs2(0, -1);
+        return res;
+
+    }
+
+    void dfs(int root, int pre) {
+        for (auto i : tree[root]) {
+            if (i == pre) continue;
+            dfs(i, root);
+            count[root] += count[i];
+            res[root] += res[i] + count[i];
+        }
+    }
+
+    void dfs2(int root, int pre) {
+        for (auto i : tree[root]) {
+            if (i == pre) continue;
+            res[i] = res[root] - count[i] + count.size() - count[i];
+            dfs2(i, root);
+        }
     }
 };
