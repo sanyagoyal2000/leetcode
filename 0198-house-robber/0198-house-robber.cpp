98d928895d1545773cc007781127011a11b7269c
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2022-12-22 13:14:16 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (78.97%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+          if(nums.size()==1)return nums[0];
+        vector<int> dp(101);
+        dp[0]=nums[0];dp[1]=max(nums[1],dp[0]); 
+        
+        for(int i=2;i<nums.size();i++){
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[nums.size()-1];
+    }
+};
