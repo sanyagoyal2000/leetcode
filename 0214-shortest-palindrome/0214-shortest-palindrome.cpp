e3b72a41e11de421eb85483e9dff2abf912ee73f
Sanya Goyal <54900358+sanyagoyal2000@users.noreply.github.com>
2023-01-22 15:31:52 +0530

    Time: 12 ms (17.73%), Space: 8 MB (34.81%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -3,31 +3,33 @@ public:
     int LPS(string s){
         int n = s.size();
         vector<int>lps(n,0);
-        int len = 0;
-        int i = 1;
-        while(i < n){
-            if(s[i] == s[len]){
+        int i=1;
+        int len=0;
+        while(i<n){
+            if(s[i]==s[len]){
                 len++;
-                lps[i] = len;
+                lps[i]=len;
                 i++;
             }
             else{
-                if(len > 0){
-                    len = lps[len-1];
+                if(len>0){
+                   len=lps[len-1]; 
                 }
                 else{
+                    lps[i]=0;
                     i++;
                 }
+                
             }
         }
         return lps[n-1];
     }
     string shortestPalindrome(string s) {
-        string t = s + "#";
+        string t = s;
+        reverse(t.begin(),t.end());
+        t=s+'#'+t;
         int n = s.size();
-        for(int i = n-1; i >= 0; i--){
-            t += s[i];
-        }
+        
         int len = LPS(t);
         string ans = "";
         for(int j = n-1; j >= len; j--){
@@ -35,5 +37,6 @@ public:
         }
         ans += s;
         return ans;
+    
     }
 };
